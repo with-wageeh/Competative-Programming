fca5f74bb05b9f61d614a89c422e38e058226ebe
Ahmed Wageeh <ahmedwageeh5040@gmail.com>
2024-08-16 15:39:14 +0300

    update

--- Geometry/Rectangle.cpp
+++ Geometry/Rectangle.cpp
@@ -1,4 +1,5 @@
-
+// to calculate the remind area after the truck maybe overlap the two triangles
+// points is the leftlower and rightupper
 struct Rect {
 	int x1, y1, x2, y2;
 	int area() { return (y2 - y1) * (x2 - x1); }
@@ -17,3 +18,29 @@ int main() {
 	cin >> t.x1 >> t.y1 >> t.x2 >> t.y2;
 	cout << a.area() + b.area() - intersect(a, t) - intersect(b, t) << endl;
 }
+
+              *************************************************
+   // to check the intersect between two rectangles
+   bool intersect(vector<int> &s1, vector<int> &s2) {
+       int bl_a_x = s1[0], bl_a_y = s1[1], tr_a_x = s1[2], tr_a_y = s1[3];
+       int bl_b_x = s2[0], bl_b_y = s2[1], tr_b_x = s2[2], tr_b_y = s2[3];
+
+       // no overlap
+       if (bl_a_x >= tr_b_x || tr_a_x <= bl_b_x || bl_a_y >= tr_b_y ||
+           tr_a_y <= bl_b_y) {
+           return false;
+       } else {
+           return true;
+       }
+
+       *************************************************
+// calculate the area of intersection
+               int inter_area(vector<int> s1, vector<int> s2) {
+           int bl_a_x = s1[0], bl_a_y = s1[1], tr_a_x = s1[2], tr_a_y = s1[3];
+           int bl_b_x = s2[0], bl_b_y = s2[1], tr_b_x = s2[2], tr_b_y = s2[3];
+
+           return ((min(tr_a_x, tr_b_x) - max(bl_a_x, bl_b_x)) *
+                   (min(tr_a_y, tr_b_y) - max(bl_a_y, bl_b_y)));
+       }
+       ***************************************************
+

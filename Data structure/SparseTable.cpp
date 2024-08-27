9bce2c25dd39986cf4953cb6065bb692bcf997f5
Ahmed Wageeh <ahmedwageeh5040@gmail.com>
2024-08-27 11:50:28 +0300

    update sp table

--- Data structure/SparseTable.cpp
+++ Data structure/SparseTable.cpp
@@ -11,7 +11,7 @@ struct  sparsetable{
         //max
         //gcd
         //lcm
-        return min(a , b);
+        return mul(a , b);
     }
     void build(int n , vector<ll> &v){
         for (int i = 0; i < n ; ++i) table[i][0] = v[i];
@@ -21,6 +21,15 @@ struct  sparsetable{
                 table[j][i] = merge(table[j][i - 1] , table[j + (1 << (i - 1))][i - 1]);
 
         for (int i = 2; i <= n ; ++i) log[i] = log[i / 2] + 1 ;
+    }
+    ll query(int l , int r){ // O(log (n))
+        // mul
+        int sz = r - l + 1;
+        ll ret = 1; // Depend on the type of operation
+        for (int i = 21; i >= 0 ; --i)
+            if (sz & (1 << i))
+                ret = merge(ret, table[l][i]) , l += (1 << i);
+        return ret;
     }
     ll Query(int l , int r){ // O(1)
         int sz = r - l + 1;
